e09f31be0de7c98f41f5fc42bed1f1ea74cfce0e
davidprager <david_prager@yahoo.com>
2021-12-04 20:38:06 -0500

    Initial commit

---
+++ validate.js
@@ -0,0 +1,51 @@
+/* This file processes a form */
+
+"use strict"
+// Define constants for error messages
+
+
+function setupForm() {
+    // Define variables for all actionable elements in the form
+
+
+    // Set up event listeners for fields on form
+
+    // This function checks that
+    function check___() {
+
+    }
+
+    // This function checks that
+    function check____() {
+
+    }
+
+
+}
+
+function validateForm() {
+    // Define variables for all actionable elements in the form
+
+
+    // Set errors variable to false.  At the end of validation, errors will still be false if there are no errors
+    let errors = false;
+
+    // Check if
+
+
+    // Check if
+
+
+    // If there are errors, return false, which will prevent the form from being submitted to the server
+    // If there are no errors, display the success message
+    if (errors) {
+        return false;
+    } else {
+        responseDisplay.style.display = "block";
+        return true;
+    }
+}
+
+
+
+
